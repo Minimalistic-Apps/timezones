2f027aadfd9626627e4be432f35969d0c4977b42
hank.milliken94@protonmail.com <Hank Milliken>
2023-01-22 03:18:04 +0100

    Figured out that I need to write select by myself using action-sheet

--- App.tsx
+++ App.tsx
@@ -1,89 +1,85 @@
-import React, { useState } from "react";
+import React from "react";
 import { SafeAreaView, ScrollView, Text, View } from "react-native";
 import { DateTime } from "luxon";
 import { Colors } from "./src/Colors";
 import { TimezoneRow } from "./src/TimezoneRow";
 import { Layout } from "./src/Layout";
 import { usePersistedState } from "./src/usePersistedState";
-import SelectDropdown from "react-native-select-dropdown";
 import { luxonValidTimezones } from "./src/getValidTimezones";
+import { SelectTimezone } from "./src/SelectTimezone";
+import { SheetProvider } from "react-native-actions-sheet";
 
 const App = () => {
   const backgroundStyle = { backgroundColor: Colors.backgroundColor() };
-  const [timezoneQuery, setTimezoneQuery] = useState<string>("");
   const date = DateTime.local({ locale: "en-GB" });
 
   const [timezones, setTimezones] = usePersistedState("timezones", ["Europe/Prague", "America/New_York"]);
 
-  const availableTimezones = Array.from(luxonValidTimezones.values()).filter(
-    (tz) => tz.name.toLowerCase().includes(timezoneQuery.toLowerCase()) && !timezones.includes(tz.name),
-  );
+  const availableTimezones = Array.from(luxonValidTimezones.values()).filter((tz) => !timezones.includes(tz.name));
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <View style={{ flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-          <View
-            style={{
-              flexDirection: "column",
-              zIndex: 0,
-              position: "absolute",
-            }}
-          >
-            {timezones.map((zoneKey) => {
-              const zone =luxonValidTimezones.get(zoneKey)
+    <SheetProvider>
+      <SafeAreaView style={backgroundStyle}>
+        <View style={{ flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
+          <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+            <View
+              style={{
+                flexDirection: "column",
+                zIndex: 0,
+                position: "absolute",
+              }}
+            >
+              {timezones.map((zoneKey) => {
+                const zone = luxonValidTimezones.get(zoneKey);
 
-              return (
-                <View style={{ flexDirection: "column" }} key={zoneKey}>
-                  <View
-                    style={{
-                      position: "relative",
-                      height: Layout.TimezoneHeaderHight,
-                      paddingStart: 4,
-                      paddingEnd: 4,
-                    }}
-                  >
-                    <Text>
-                      {zone?.name} | {zone?.abbreviation}
-                    </Text>
+                return (
+                  <View style={{ flexDirection: "column" }} key={zoneKey}>
+                    <View
+                      style={{
+                        position: "relative",
+                        height: Layout.TimezoneHeaderHight,
+                        paddingStart: 4,
+                        paddingEnd: 4,
+                      }}
+                    >
+                      <Text>
+                        {zone?.name} | {zone?.abbreviation}
+                      </Text>
+                    </View>
+                    <View style={{ flexDirection: "row", height: Layout.TimezoneHight }} />
                   </View>
-                  <View style={{ flexDirection: "row", height: Layout.TimezoneHight }} />
+                );
+              })}
+            </View>
+            <View style={{ zIndex: 1 }}>
+              <ScrollView horizontal>
+                <View style={{ paddingBottom: 8 }}>
+                  {timezones.map((zone) => (
+                    <TimezoneRow
+                      date={date}
+                      zone={zone}
+                      key={zone}
+                      onDelete={() => {
+                        setTimezones(timezones.filter((tz) => tz !== zone));
+                      }}
+                    />
+                  ))}
                 </View>
-              );
-            })}
+              </ScrollView>
+            </View>
+          </ScrollView>
+          <View style={{ flexDirection: "row", width: "100%", backgroundColor: "blue" }}>
+            <SelectTimezone
+              data={availableTimezones}
+              selected={null}
+              onSelect={(value) => {
+                setTimezones([...new Set([...timezones, value.name])]);
+              }}
+            />
           </View>
-          <View style={{ zIndex: 1 }}>
-            <ScrollView horizontal>
-              <View style={{ paddingBottom: 8 }}>
-                {timezones.map((zone) => (
-                  <TimezoneRow
-                    date={date}
-                    zone={zone}
-                    key={zone}
-                    onDelete={() => {
-                      setTimezones(timezones.filter((tz) => tz !== zone));
-                    }}
-                  />
-                ))}
-              </View>
-            </ScrollView>
-          </View>
-        </ScrollView>
-        <View style={{ flexDirection: "row" }}>
-          <SelectDropdown
-            buttonStyle={{ width: "100%", borderRadius: 10 }}
-            rowStyle={{ alignItems: "flex-start", justifyContent: "flex-start" }}
-            search={true}
-            data={availableTimezones.map(tz => `${tz.name} (${tz.abbreviation})`)}
-            onSelect={(value) => {
-              setTimezones([...new Set([...timezones, value])]);
-            }}
-            onChangeSearchInputText={setTimezoneQuery}
-            onBlur={() => setTimezoneQuery("")}
-          />
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </SheetProvider>
   );
 };
 
